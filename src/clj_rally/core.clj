58dcf705c376b8854fecdd5fec6e4d682e40faa4
nmusaelian-rally <nmusaelian@rallydev.com>
2018-08-06 17:01:59 -0600

    started refactor

--- src/clj_rally/core.clj
+++ src/clj_rally/core.clj
@@ -1,10 +1,10 @@
 (ns clj-rally.core
-    (:gen-class)
-    (:require [clj-http.client :as client]
-              [clojure.data.json :as json]
-              [clojure.edn]
-              [clojure.string :as str]
-              [clj-time.core :as t]))
+  (:gen-class)
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]
+            [clojure.edn]
+            [clojure.string :as str]
+            [clj-time.core :as t]))
 
 (defn make-request
   [method headers base-url endpoint payload log-level]
@@ -24,15 +24,22 @@
 
 (def config (clojure.edn/read-string (slurp "resources/config.edn")))
 
-(defn read-config [config]
-  (:rally config))
+(def rally (get-in config [:rally]))
 
-(defn context
-  [workspaces-url headers base-url]
+(defn subscription-info []
+  (let [ sub-endpoint "subscription?fetch=subscriptionID,workspaces" empty-payload ""
+        result (make-request :get (get-in rally [:auth]) (get-in rally [:base-url]) sub-endpoint empty-payload (get-in rally [:log-level]))
+        sub-info {:sub-id         (get-in (json/read-str result) ["Subscription" "SubscriptionID"])
+                  :sub-uuid       (get-in (json/read-str result) ["Subscription" "_refObjectUUID"])
+                  :workspaces-url (second (str/split (get-in (json/read-str result) ["Subscription" "Workspaces" "_ref"]) #"v2.0" ))}]
+    sub-info))
+
+(defn context []
   (def payload "")
   (def log-level "info")
-  (def workspaces-resource (second (str/split workspaces-url #"v2.0"))) ; /Subscription/123/Workspaces
-  (def rally (read-config config))
+  (def headers   (get-in rally [:auth]))
+  (def base-url  (get-in rally [:base-url]))
+  (def workspaces-resource (second (str/split (get-in subscription-info [:workspaces-url]) #"v2.0"))) ; /Subscription/123/Workspaces
   (def workspace-name (get-in rally [:workspace]))
   (def project-name   (get-in rally [:project]))
   (def workspace-query (format "?query=(Name = \"%s\")&fetch=ObjectID,Projects" workspace-name))
@@ -43,33 +50,44 @@
   (def projects-resource (second (str/split projects-url #"v2.0"))) ; /Workspace/456/Projects
   (def project-query (format "?query=(Name = \"%s\")&fetch=ObjectID" project-name))
   (def project-endpoint (str projects-resource project-query))
-  (print project-endpoint)
   (def proj-result (make-request :get headers base-url project-endpoint payload log-level))
   (def project-oid (get-in (json/read-str wrk-result) ["QueryResult" "Results" 0 "ObjectID"]))
   {:workspace workspace-oid :project project-oid}
   )
 
-(defn -main [& args]
-  (def rally (read-config config))
-  (def headers   (get-in rally [:auth]))
-  (def base-url  (get-in rally [:base-url]))
-  (def workspace (get-in rally [:workspace]))
-  (def project   (get-in rally [:project]))
-  (def log-level (get-in rally [:log-level]))
 
-  (def empty-payload "")
+(def default-options
+  (memoize
+    (fn []
+      {:context (context)})
+    ))
 
-  (def sub-endpoint "subscription?fetch=subscriptionID,workspaces")
-  (def result   (make-request :get headers base-url sub-endpoint empty-payload log-level))
-  (def sub-id   (get-in (json/read-str result) ["Subscription" "SubscriptionID"]))
-  (def sub-uuid (get-in (json/read-str result) ["Subscription" "_refObjectUUID"]))
-  (def workspaces-url (get-in (json/read-str result) ["Subscription" "Workspaces" "_ref"]))
-  (def sub (str sub-id " " sub-uuid))
-  (def context-oids (context workspaces-url headers base-url))
-  (def create-endpoint (format "/story/create?workspace=/workspace/%s&project=/project/%s" (get context-oids :workspace) (get context-oids :project)))
+(defn extended-options [& options]
+  (let [options (merge default-options options)]
+    options))
 
-  (def payload (format "{\"HierarchicalRequirement\":
-    {\"Name\":\"clojure story %s\"}}" (str (t/time-now))))
-
-  (println(make-request :post (assoc headers :subscription sub) base-url create-endpoint payload log-level))
-  )
+;(defn custom-header
+;  []
+;  (def sub (subscription-info))
+;  (assoc (get-in rally [:auth]) :subscription sub)
+;  )
+;
+;(defn -main [& args]
+;  ;(def rally (read-config config))
+;  (def headers   (get-in rally [:auth]))
+;  (def base-url  (get-in rally [:base-url]))
+;  (def workspace (get-in rally [:workspace]))
+;  (def project   (get-in rally [:project]))
+;  (def log-level (get-in rally [:log-level]))
+;  (def empty-payload "")
+;  (def workspaces-url (get-in (json/read-str result) ["Subscription" "Workspaces" "_ref"]))
+;  (def sub (str sub-id " " sub-uuid))
+;  (def context-oids (context))
+;  (def create-endpoint (format "/story/create?workspace=/workspace/%s&project=/project/%s" (get context-oids :workspace) (get context-oids :project)))
+;
+;  (def payload (format "{\"HierarchicalRequirement\":
+;    {\"Name\":\"clojure story %s\"}}" (str (t/time-now))))
+;
+;  ;(println(make-request :post (assoc headers :subscription sub) base-url create-endpoint payload log-level))
+;  (println(make-request :post (subscription-info) base-url create-endpoint payload log-level))
+;  )
--- test/clj_rally/core_test.clj
+++ test/clj_rally/core_test.clj
@@ -1,13 +1,20 @@
 (ns clj-rally.core-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as str]
             [clj-rally.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest subscription-info-test
+  (let [result (subscription-info)]
+    (is (= (get-in result [:workspaces-url]) "/Subscription/1154643/Workspaces"))
+    (is (= (get-in result [:sub-id]) 209))
+    (is (= (get-in result [:sub-uuid]) "13eb4d62-3c4f-442e-b825-9f6786726d99")))
+  )
 
 
-(a-test)
+(defn -main [& args]
+  (subscription-info-test)
+  )
 
 
 
