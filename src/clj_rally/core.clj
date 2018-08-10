9b6b399103b0d0b2cb06ae76235549b3f6d03eb3
nmusaelian-rally <nmusaelian@rallydev.com>
2018-08-10 16:24:20 -0600

    created convenience functions, made make-request private

--- src/clj_rally/core.clj
+++ src/clj_rally/core.clj
@@ -15,7 +15,7 @@
   [config]
   (get-in config [:rally]))
 
-(defn make-request
+(defn- make-request
   "Function for making CRUD http requests"
   [method headers endpoint & {:keys [payload query]}]
   (log/info (format "making request: %s" endpoint))
@@ -41,8 +41,9 @@
       (log/error (str "Oh noes! Method not allowed 401! Body:\n" body)))
     ))
 
-(defn subscription-info []
+(defn subscription-info
   "Makes a GET request to return a hashmap of sub-id, sub-uuid and workspaces-url"
+  []
   (let [ sub-endpoint "subscription?fetch=subscriptionID,workspaces"
         result (make-request :get (get-in rally [:auth]) sub-endpoint)
         sub-info {:sub-id         (get-in (json/read-str result) ["Subscription" "SubscriptionID"])
@@ -50,8 +51,9 @@
                   :workspaces-url (second (str/split (get-in (json/read-str result) ["Subscription" "Workspaces" "_ref"]) #"v2.0" ))}]
     sub-info))
 
-(defn context []
+(defn context
   "Makes a GET request to return a hashmap of OIDs for workspace and project specified in config."
+  []
   (let [headers (get-in rally [:auth])
         wrk-resource (get-in (subscription-info) [:workspaces-url])
         wrk-query    (format "&query=(Name = \"%s\")" (get-in rally [:workspace]))
@@ -69,3 +71,42 @@
     {:workspace wrk-oid :project prj-oid})
   )
 
+
+(defn create-workitem
+  "Takes two arguments: work item type and hashmap of the payload, e.g. {:Name name :PlanEstimate est}"
+  [wi-type data]
+  (let [record {(keyword wi-type) data}
+        payload     (json/write-str record)
+        context-oids (context)
+        resource    "/%s/create?workspace=/workspace/%s&project=/project/%s"
+        endpoint   (format resource wi-type (get context-oids :workspace) (get context-oids :project))
+        result     (make-request :post (get-in rally [:auth]) endpoint :payload payload)]
+    result))
+
+(defn read-workitem
+  "Takes three arguments: work item type, query string, e.g. (Name = \"DON'T DELETE\") and fetch string, e.g ObjectID,ScheduleState."
+  [wi-type query-str fetch-str]
+  (let [query (str "&query=" query-str)
+        fetch (str "&fetch=" fetch-str)
+        context-oids (context)
+        resource "/%s?workspace=/workspace/%s&project=/project/%s"
+        endpoint (format (str resource fetch) wi-type (get context-oids :workspace) (get context-oids :project))
+        result (make-request :get (get-in rally [:auth]) endpoint :query query)]
+    result)
+  )
+
+(defn update-workitem
+  "Takes three arguments: work item type, hashmap of the payload, e.g {:ScheduleState target-state} and ObjectID of workitem"
+  [wi-type data oid]
+  (let [record {(keyword wi-type) data}
+        payload     (json/write-str record)
+        endpoint   (format "/%s/%s" wi-type oid)
+        result     (make-request :post (get-in rally [:auth]) endpoint :payload payload)]
+    result))
+
+(defn delete-workitem
+  "Takes two arguments: work item type and ObjectID of workitem"
+  [wi-type oid]
+  (let [endpoint   (format "/%s/%s" wi-type oid)
+        result     (make-request :delete (get-in rally [:auth]) endpoint)]
+    result))
--- test/clj_rally/core_test.clj
+++ test/clj_rally/core_test.clj
@@ -6,6 +6,14 @@
             [clojure.tools.logging :as log]
             [clj-time.core :as t]))
 
+(defn random-name
+  []
+  (let [adjec  (vector "fatuous", "optimistic", "officious", "unctuous")
+        nouns  (vector "troglodyte", "fantomas", "pony", "puffery")
+        rand1  (rand-int (count adjec))
+        rand2   (rand-int (count nouns))
+        name    (str/join " " [(get adjec rand1) (get nouns rand2) (str (t/time-now))])]
+    name))
 
 (deftest subscription-info-test
   (let [result (subscription-info)]
@@ -18,42 +26,83 @@
     (is (= (get-in result [:workspace]) 1572380957))
     (is (= (get-in result [:project]) 1572381037))))
 
-(deftest create-n-delete-test
-  (let [artifact-type "HierarchicalRequirement"
-        payload (format "{\"%s\": {\"Name\":\"another clojure toolkit story %s\"}}" artifact-type (str (t/time-now)))
-        context-oids (context)
-        story-resource "/%s/create?workspace=/workspace/%s&project=/project/%s"
-        create-endpoint (format story-resource artifact-type (get context-oids :workspace) (get context-oids :project))]
-    (let [result (make-request :post (get-in rally [:auth]) create-endpoint :payload payload)
-          story-name (get-in (json/read-str result) ["CreateResult" "Object" "_refObjectName"])
-          story-ref (get-in (json/read-str result) ["CreateResult" "Object" "_ref"])]
-      (is (str/starts-with? story-name "another"))
-      (let [story-endpoint (second (str/split story-ref #"v2.0"))
-            delete-result (make-request :delete (get-in rally [:auth]) story-endpoint)
-            errors (get-in (json/read-str delete-result) ["OperationResult" "Errors"])]
-        (is (empty? errors))))))
+;(deftest create-n-delete-test
+;  (let [artifact-type "HierarchicalRequirement"
+;        payload (format "{\"%s\": {\"Name\":\"another clojure toolkit story %s\"}}" artifact-type (str (t/time-now)))
+;        context-oids (context)
+;        story-resource "/%s/create?workspace=/workspace/%s&project=/project/%s"
+;        create-endpoint (format story-resource artifact-type (get context-oids :workspace) (get context-oids :project))]
+;    (let [result (make-request :post (get-in rally [:auth]) create-endpoint :payload payload)
+;          story-name (get-in (json/read-str result) ["CreateResult" "Object" "_refObjectName"])
+;          story-ref (get-in (json/read-str result) ["CreateResult" "Object" "_ref"])]
+;      (is (str/starts-with? story-name "another"))
+;      (let [story-endpoint (second (str/split story-ref #"v2.0"))
+;            delete-result (make-request :delete (get-in rally [:auth]) story-endpoint)
+;            errors (get-in (json/read-str delete-result) ["OperationResult" "Errors"])]
+;        (is (empty? errors))))))
 
-(deftest find-n-update-test
-  (let [artifact-type "HierarchicalRequirement"
-        query  "&query=(Name = \"DON'T DELETE\")"
-        fetch  "&fetch=ObjectID,ScheduleState"
-        context-oids (context)
-        story-resource "/%s?workspace=/workspace/%s&project=/project/%s"
-        find-endpoint (format (str story-resource fetch) artifact-type (get context-oids :workspace) (get context-oids :project))]
-    (let [result (make-request :get (get-in rally [:auth]) find-endpoint :query query)
-          story-name  (get-in (json/read-str result) ["QueryResult" "Results" 0 "_refObjectName"])
-          story-ref   (get-in (json/read-str result) ["QueryResult" "Results" 0 "_ref"])
-          story-state (get-in (json/read-str result) ["QueryResult" "Results" 0 "ScheduleState"])]
-      (is (str/starts-with? story-name "DON'T DELETE"))
-      (log/info story-ref)
+;(deftest find-n-update-test
+;  (let [artifact-type "HierarchicalRequirement"
+;        query  "&query=(Name = \"DON'T DELETE\")"
+;        fetch  "&fetch=ObjectID,ScheduleState"
+;        context-oids (context)
+;        story-resource "/%s?workspace=/workspace/%s&project=/project/%s"
+;        find-endpoint (format (str story-resource fetch) artifact-type (get context-oids :workspace) (get context-oids :project))]
+;    (let [result (make-request :get (get-in rally [:auth]) find-endpoint :query query)
+;          story-name  (get-in (json/read-str result) ["QueryResult" "Results" 0 "_refObjectName"])
+;          story-ref   (get-in (json/read-str result) ["QueryResult" "Results" 0 "_ref"])
+;          story-state (get-in (json/read-str result) ["QueryResult" "Results" 0 "ScheduleState"])]
+;      (is (str/starts-with? story-name "DON'T DELETE"))
+;      (log/info story-ref)
+;    (let [target-state (if (= story-state "Defined") "In-Progress" "Defined")
+;          update-payload  (format "{\"%s\":{\"ScheduleState\":\"%s\"}}" artifact-type target-state)
+;          update-endpoint (second (str/split story-ref #"v2.0"))]
+;      (let [update-result (make-request :post (get-in rally [:auth]) update-endpoint :payload update-payload)
+;            updated-story-state (get-in (json/read-str update-result) ["OperationResult" "Object" "ScheduleState"])]
+;        (is (= updated-story-state target-state)))))))
 
-    (let [target-state (if (= story-state "Defined") "In-Progress" "Defined")
-          update-payload  (format "{\"%s\":{\"ScheduleState\":\"%s\"}}" artifact-type target-state)
-          update-endpoint (second (str/split story-ref #"v2.0"))]
-      (let [update-result (make-request :post (get-in rally [:auth]) update-endpoint :payload update-payload)
-            updated-story-state (get-in (json/read-str update-result) ["OperationResult" "Object" "ScheduleState"])]
-        (is (= updated-story-state target-state)))))))
+(deftest create-test
+  (let [wi-type    "HierarchicalRequirement"
+        name       (random-name)
+        est        (float(+ (rand-int 10) 1))
+        data       {:Name name :PlanEstimate est}
+        result     (create-workitem wi-type data)
+        story-name (get-in (json/read-str result) ["CreateResult" "Object" "Name"])
+        story-est  (get-in (json/read-str result) ["CreateResult" "Object" "PlanEstimate"])]
+    (is (= story-name name))
+    (is (= story-est est))))
 
+(deftest read-test
+  (let [wi-type    "HierarchicalRequirement"
+        query      "(Name = \"DON'T DELETE\")"
+        fetch      "ObjectID,ScheduleState"
+        result     (read-workitem wi-type query fetch)
+        name       (get-in (json/read-str result) ["QueryResult" "Results" 0 "_refObjectName"])]
+    (is (= name "DON'T DELETE"))))
+
+(deftest update-test
+  (let [wi-type      "HierarchicalRequirement"
+        query        "(Name = \"DON'T DELETE\")"
+        fetch        "ObjectID,ScheduleState"
+        result       (read-workitem wi-type query fetch)
+        oid          (get-in (json/read-str result) ["QueryResult" "Results" 0 "ObjectID"])
+        state        (get-in (json/read-str result) ["QueryResult" "Results" 0 "ScheduleState"])
+        target-state (if (= state "Defined") "In-Progress" "Defined")
+        data         {:ScheduleState target-state}
+        new-result   (update-workitem wi-type data oid)
+        new-state    (get-in (json/read-str new-result) ["OperationResult" "Object" "ScheduleState"])]
+    (is (= new-state target-state))))
+
+(deftest delete-test
+  (let [wi-type    "HierarchicalRequirement"
+        name       (random-name)
+        est        (float(+ (rand-int 10) 1))
+        data       {:Name name :PlanEstimate est}
+        result     (create-workitem wi-type data)
+        oid        (get-in (json/read-str result) ["CreateResult" "Object" "ObjectID"])
+        deleted    (delete-workitem wi-type oid)
+        errors     (get-in (json/read-str deleted) ["OperationResult" "Errors"])]
+    (is (empty? errors))))
 
 (deftest bad-creds-test
   (is (thrown? Exception (subscription-info))))
@@ -67,8 +116,12 @@
 
   (subscription-info-test)
   (context-test)
-  (create-n-delete-test)
-  (find-n-update-test)
+  ;(create-n-delete-test)
+  ;(find-n-update-test)
+  (create-test)
+  (read-test)
+  (update-test)
+  (delete-test)
 
   (let [config (clojure.edn/read-string (slurp "resources/bad-creds.edn"))]
     (intern 'clj-rally.core 'rally (connection config)))
